d30973cf0901157c2ef6ff0213027d568f546a4d
gabrielbaute <gabrielbaute@gmail.com>
2026-03-22 16:42:17 -0400

    Agregamos la versión de la app como variable global cuando se compile

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,3 +5,6 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+// Declaración global de la versión de la app
+declare const __APP_VERSION__: string;
