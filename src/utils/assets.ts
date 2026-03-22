5f26fa49d4fac1945fdc15bc323a39446128a031
gabrielbaute <gabrielbaute@gmail.com>
2026-03-22 14:59:04 -0400

    Implementamos un Asset Manager para manejar las rutas de los iconos y logos, y centralizarlas

---
+++ src/utils/assets.ts
@@ -0,0 +1,13 @@
+export const AppAssets = {
+  // Isologos
+  isologoAzul: new URL('../assets/img/isologo_azul.png', import.meta.url).href,
+  isologoDorado: new URL('../assets/img/isologo_dorado.png', import.meta.url).href,
+  isologoGris: new URL('../assets/img/isologo_gris.png', import.meta.url).href,
+  isologoNegro: new URL('../assets/img/isologo_negro.png', import.meta.url).href,
+
+  // Logos
+  logoAzul: new URL('../assets/img/logo_azul.png', import.meta.url).href,
+  logoDorado: new URL('../assets/img/logo_dorado.png', import.meta.url).href,
+  logoGris: new URL('../assets/img/logo_gris.png', import.meta.url).href,
+  logoNegro: new URL('../assets/img/logo_negro.png', import.meta.url).href,
+};
